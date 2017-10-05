ce5ba4664cc463213afc1619d7d07d181b63d278
txrxrxr <xtie@ualberta.ca>
2017-10-04 18:45:46 -0600

    ElasticSearch

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/NormalTweet.java
@@ -1,6 +1,12 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import java.util.Date;
+
 public class NormalTweet extends Tweet {
+
+    public NormalTweet (String message, Date date) {
+        super(message, date);
+    }
 
     public NormalTweet(String message) {
         super(message);
--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,10 +1,21 @@
 package ca.ualberta.cs.lonelytwitter;
 
+import io.searchbox.annotations.JestId;
 import java.util.Date;
 
 public abstract class Tweet implements Tweetable {
     private String message;
     private Date date;
+    @JestId
+    private String id;
+
+    public  String getId() {
+        return id;
+    }
+
+    public void setId (String id) {
+        this.id = id;
+    }
 
     public Tweet(String message){
         this.message = message;
